4ef05379f894a85f4df8b9cee3c10996b8daa2b9
Mohanad Godzilla <39066284+MohanedGodzilla@users.noreply.github.com>
2022-05-29 14:52:08 +0200

    adding a validation with Joi to the routes

--- src/routes/machine.js
+++ src/routes/machine.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const Coffee_Machine = require('../models/machine')
+const Joi = require('joi');
+const validator = require('express-joi-validation').createValidator({})
 
-router.get('/', async (req,res)=>{
-    let size = req.query.size;
-    let waterLine= req.query.water_line;
+const querySchema = Joi.object().keys({
+    size: Joi.string().valid('COFFEE_MACHINE_LARGE', 'COFFEE_MACHINE_SMALL', 'ESPRESSO_MACHINE'),
+    water_line: Joi.boolean()
+  }).required().min(1)
+  
+router.get('/', validator.query(querySchema), async (req,res)=>{
+    const size = req.query.size;
+    const waterLine= req.query.water_line;
     let Query = [];
     if(size)
         Query.push({product_type: size});
--- src/routes/pods.js
+++ src/routes/pods.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const Coffee_Pods = require('../models/pods')
+const Joi = require('joi');
+const validator = require('express-joi-validation').createValidator({})
 
-router.get('/', async (req,res)=>{
-    let size = req.query.size;
-    let coffeeFlavor= req.query.coffee_flavor;
-    let packSize= req.query.pack_size;
+const querySchema = Joi.object().keys({
+    size: Joi.string().valid(
+        'COFFEE_POD_LARGE', 
+        'COFFEE_POD_SMALL', 
+        'ESPRESSO_POD'),
+    coffee_flavor: Joi.string().valid(
+        'COFFEE_FLAVOR_VANILLA', 
+        'COFFEE_FLAVOR_CARAMEL', 
+        'COFFEE_FLAVOR_MOCHA', 
+        'COFFEE_FLAVOR_PSL', 
+        'COFFEE_FLAVOR_HAZELNUT'),
+    pack_size: Joi.number().valid(1,3,5,7)
+  }).required().min(1)
+
+
+router.get('/', validator.query(querySchema), async (req,res)=>{
+    const size = req.query.size;
+    const coffeeFlavor= req.query.coffee_flavor;
+    const packSize= req.query.pack_size;
     let Query = [];
     if(size)
         Query.push({product_type: size});
